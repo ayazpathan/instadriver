ff5bcd3c4aa8f88481985865dc4b0b40046708b9
Ayaz Pathan <pathan.ayaz@outlook.com>
2020-11-29 16:47:41 +0530

    Completed NAV menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { FaCar, FaSearch, FaMailBulk, FaSuitcase, FaMosque, FaRegStar, FaCcMastercard } from "react-icons/fa";
+import { MdPermIdentity } from "react-icons/md";
 
 function App() {
   return (
@@ -12,7 +14,16 @@ function App() {
         </div>
         <div className="body_wrapper">
           <div className="nav_menu">
-          
+              <ul>
+                <li><MdPermIdentity/> <span>Employer Profile</span></li>
+                <li><FaCar/> <span>Onboard Your Vehicle</span></li>
+                <li><FaSearch/> <span>Search & Hire Drivers</span></li>
+                <li><FaMailBulk /><span>Inbox</span></li>
+                <li><FaSuitcase/><span>Recruitment</span></li>
+                <li><FaMosque/> <span>My Organisation</span></li>
+                <li><FaRegStar/> <span>Rate A Driver</span></li>
+                <li><FaCcMastercard/> <span>My Subscription</span></li>
+              </ul>
           </div>
           <div className="body_content_wrapper">
             <div className="chat_wrapper">
